f090f286b5b44721a47e40b984f37d1a9fe2f194
Mugilan <mugilankani951@gmail.com>
2023-12-01 21:36:23 -0800

    fdinished

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+const playButton = document.querySelector('.play')
+playButton.addEventListener('click', ()=>{
+  location.href="game.html"
+})
